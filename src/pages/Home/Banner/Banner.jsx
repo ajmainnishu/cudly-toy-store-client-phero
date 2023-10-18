28dd35375bba8238c452815fb625b3483f308723
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-18 19:15:09 +0600

    feat: Banner component added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,9 +1,9 @@
-
+import banner from '../../../assets/banner/banner.jpg';
 
 const Banner = () => {
     return (
         <div>
-            
+            <img className='w-100' src={banner} alt="banner" />
         </div>
     );
 };
