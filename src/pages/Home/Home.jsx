48ff2fc6abcccd33363e7e82a8abb3964acec4d2
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-18 23:23:12 +0600

    feat: BuyKids component added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "./Banner/Banner";
+import BuyKids from "./BuyKids/BuyKids";
 import Gallery from "./Gallery/Gallery";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Gallery></Gallery>
+            <BuyKids></BuyKids>
         </div>
     );
 };
