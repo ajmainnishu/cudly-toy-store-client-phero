16e82a0c3758e490b01f68d9e92cf1014042ccd9
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-18 19:45:12 +0600

    feat: Footer component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navigation from './shared/Navigation'
+import Footer from './shared/Footer'
 
 function App() {
   return (
     <div>
       <Navigation></Navigation>
       <Outlet></Outlet>
+      <Footer />
     </div>
   )
 }
--- src/shared/Navigation.jsx
+++ src/shared/Navigation.jsx
@@ -10,6 +10,7 @@ const Navigation = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
+                        {/* small device */}
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                             <li><Link to={`/`}>Home</Link></li>
                             <li><Link>All Toys</Link></li>
@@ -18,11 +19,13 @@ const Navigation = () => {
                             <li><Link>Blogs</Link></li>
                         </ul>
                     </div>
+                    {/* website logo */}
                     <Link to={`/`} className="text-xl">
                         <img src={logo} alt="website logo" />
                     </Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
+                    {/* large device */}
                     <ul className="menu menu-horizontal px-1">
                         <li><Link to={`/`}>Home</Link></li>
                         <li><Link>All Toys</Link></li>
@@ -31,6 +34,7 @@ const Navigation = () => {
                         <li><Link>Blogs</Link></li>
                     </ul>
                 </div>
+                {/* button */}
                 <div className="navbar-end">
                     <Link className="btn">Login</Link>
                 </div>
