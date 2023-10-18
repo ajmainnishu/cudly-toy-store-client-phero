283ceabb53bbd19e88617649ab5696f35c438c49
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-18 21:36:55 +0600

    feat: Registration component added

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Contact/Login/Login";
+import Registration from "../pages/Contact/Registration/Registration";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
             {
                 path: 'login',
                 element: <Login />
+            },
+            {
+                path: 'registration',
+                element: <Registration />
             }
         ]
     }
